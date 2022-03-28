f94a2b272886868187715c46a7ff012874ab75e9
Aamir Kamrul Islam <aamirkislam@gmail.com>
2022-03-27 22:03:01 -0400

    add VideoStatus type (#3)

--- services/townService/src/CoveyTypes.ts
+++ services/townService/src/CoveyTypes.ts
@@ -14,3 +14,15 @@ export type ChatMessage = {
   body: string;
   dateCreated: Date;
 };
+
+/**
+ * VideoStatus represents the state of a video.
+ */
+export type VideoStatus = {
+  // The URL of the video.
+  url: string;
+  // The timestamp of the video, in the format "HH:MM:SS".
+  timestamp: string;
+  // Whether or not the video is currently paused.
+  isPaused: boolean;
+};
