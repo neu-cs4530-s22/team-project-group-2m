8e5632b2363f9e88512435c05c9ede077894ab24
papadopoulosp <papadopoulos.p@northeastern.edu>
2022-03-29 16:29:10 -0400

    add viewing area folder, video player interface

---
+++ frontend/src/components/ViewingArea/VideoPlayer.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents the metadata for the video player component
+ */
+export interface VideoPlayer {
+  /** Sets the url of the current video * */
+  setURL(url: string): boolean;
+  /** Sets the timestamp of the current video * */
+  setTimestamp(timestamp: string): boolean;
+  /** Pauses or plays the currently video * */
+  setIsPaused(isPaused: boolean): boolean;
+  /** A component representing this VideoPlayer * */
+  videoComponent(): JSX.Element;
+}
