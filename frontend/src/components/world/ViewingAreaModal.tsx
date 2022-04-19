12ad545c80298bdf111a6fba8519936dd74787bc
Aamir Islam <aamirkislam@gmail.com>
2022-04-19 16:41:20 -0400

    add padding in between play/pause button and progress bar

--- frontend/src/components/world/ViewingAreaModal.tsx
+++ frontend/src/components/world/ViewingAreaModal.tsx
@@ -98,6 +98,7 @@ export default function ViewingAreaModal({
               }
             }}
           />
+          <div style={{ width: 10 }}/>
           {videoStatus && (
             <ProgressBar
               secondsElapsed={videoStatus.elapsed}
